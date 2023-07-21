d5ee072360ae52aef73f77c563e0801e4ce6fa59
sony1011 <101799428+sony1011@users.noreply.github.com>
2023-07-21 14:39:24 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ubuntu/server
-
-#_Update_&_Set_Node_Version
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-
-#_Download_Node_&NPM
-sudo apt install -y nodejs npm
-
-#_Download_PM2
-npm install pm2@latest -g
+# Stop Apache server
+sudo service apache2 stop
